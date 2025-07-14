226f16b217349a4d256345e18ee0e731f8fea03b
an90ass <anasalmaqtari@visight.com.tr>
2025-07-14 13:27:53 +0300

    alt yapi eklendi

--- sinavlab/backend/main.py
+++ sinavlab/backend/main.py
@@ -1,1 +1,11 @@
-hjhgjhhgfgf
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"home endpoint"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
