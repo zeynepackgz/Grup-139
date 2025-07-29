c66400b25062d19093a3ca30d6173168951443d2
Zeynep Açıkgöz <108180721+zeynepackgz@users.noreply.github.com>
2025-07-29 16:10:29 +0300

    main.py

--- sinavlab/backend/main.py
+++ sinavlab/backend/main.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from typing import List
-from pydantic import BaseModel
 from pathlib import Path
 import json
 
@@ -10,24 +7,6 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"home endpoint"}
-
-
-# Pydantic modeli — JSON'daki alanlara göre
-class BilgiKart(BaseModel):
-    id: int
-    title: str
-    description: str
-    
-#bilgi kart endpoint
-@app.get("/api/bilgi-kartlari", response_model=List[BilgiKart])
-def get_bilgi_kartlari():
-    dosya_yolu = Path("data/bilgi-kart.json")
-    if dosya_yolu.exists():
-        with open(dosya_yolu, "r", encoding="utf-8") as f:
-            kartlar = json.load(f)
-        return kartlar
-    else:
-        return []
 
 
 if __name__ == "__main__":
